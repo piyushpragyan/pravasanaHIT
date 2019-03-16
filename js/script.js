a8f312dc5fd660242804ff5cdce768615f38316c
raunak-sharma <raunak.sharma.cse20@heritageit.edu.in>
2019-03-16 19:27:29 +0530

    added loader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 (function () {
-
+    // to hide the loader
+    window.addEventListener("load", function () {
+        const loader = document.querySelector(".loader");
+        loader.className += " hidden";
+    });
 })();
